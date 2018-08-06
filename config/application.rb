e551e4f739a0e5c9d5d01324033590aae996c5df
David Miotti <david@muxumuxu.com>
2018-08-06 13:54:20 +0200

    Revert "[+] Use memcache"
    This reverts commit a3b3357be915a21069a4e64c71ea929019e4e866.

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,6 @@ module PregnantFood
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.use Rack::Throttle::Interval, :min => 10.0, :cache => Memcached.new, :key_prefix => :throttle
+    config.middleware.use Rack::Throttle::Interval, :min => 10.0
   end
 end
